38bdfd3e769048ee60ae1c59b83255c517209dee
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-03-16 23:32:07 -0400

    got ang material working

--- src/selfStarted/features/dashboard/dashboard.controller.js
+++ src/selfStarted/features/dashboard/dashboard.controller.js
@@ -2,6 +2,22 @@ angular
     .module('selfStarted.dashboard')
     .controller('DashboardCtrl', DashboardCtrl);
 
-function DashboardCtrl() {
+function DashboardCtrl($scope) {
     var vm = this;
+
+    var tabs = [
+        { title: 'One', content: "Tabs will become paginated if there isn't enough room for them." },
+        { title: 'Two', content: "You can swipe left and right on a mobile device to change tabs." },
+        { title: 'Three', content: "You can bind the selected tab via the selected attribute on the md-tabs element." },
+        { title: 'Four', content: "If you set the selected tab binding to -1, it will leave no tab selected." },
+    ];
+
+    var selected = null, previous = null;
+
+    $scope.tabs = tabs;
+    $scope.selectedIndex = 2;
+    $scope.$watch('selectedIndex', function (current, old) {
+        previous = selected;
+        selected = tabs[current];
+    });
 }
