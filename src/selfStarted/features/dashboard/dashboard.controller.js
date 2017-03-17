7d0a9e064cb862d8270eed9d5b6f969748efb702
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-03-17 01:38:15 -0400

    dashboard sort of working

--- src/selfStarted/features/dashboard/dashboard.controller.js
+++ src/selfStarted/features/dashboard/dashboard.controller.js
@@ -2,6 +2,10 @@ angular
     .module('selfStarted.dashboard')
     .controller('DashboardCtrl', DashboardCtrl);
 
-function DashboardCtrl($scope) {
+function DashboardCtrl($scope, $mdSidenav) {
     var vm = this;
+
+    $scope.openLeftMenu = function () {
+        $mdSidenav('left').toggle();
+    };
 }
