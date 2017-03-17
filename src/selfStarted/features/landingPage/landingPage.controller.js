88f3624fc7eed23b7bff1224b389f85fd3e9b989
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-03-16 23:47:08 -0400

    fixed error with style rendering for material css

--- src/selfStarted/features/landingPage/landingPage.controller.js
+++ src/selfStarted/features/landingPage/landingPage.controller.js
@@ -1,16 +1,9 @@
-(function(){
-
 angular
     .module('selfStarted.landingPage')
     .controller('LandingPageCtrl', LandingPageCtrl);
 
 function LandingPageCtrl($scope) {
     var vm = this;
-
-
     $scope.currentNavItem = 'page1';
 
-
 }
-
-})();
