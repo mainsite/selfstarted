74230bbbdcf7e82a8340d8177889ed1a68ae10a2
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-03-18 13:29:03 -0400

    changes

--- src/selfStarted/features/components/createProjects/createProjects.directive.js
+++ src/selfStarted/features/components/createProjects/createProjects.directive.js
@@ -22,50 +22,6 @@ function CreateProjectsCtrl($scope){
 
     var vm = this;
 
-
-    $scope.user = {
-          title: 'Developer',
-          email: 'ipsum@lorem.com',
-          firstName: '',
-          lastName: '',
-          company: 'Google',
-          address: '1600 Amphitheatre Pkwy',
-          city: 'Mountain View',
-          state: 'CA',
-          biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
-          postalCode: '94043'
-        };
-
-        $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
-        'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
-        'WY').split(' ').map(function(state) {
-            return {abbrev: state};
-          });
-
-        // Configure a dark theme with primary foreground yellow
-       
-
-        $scope.colleges = Object.keys(college);
-        $scope.selectedCollege;
-        $scope.getSelectedText = function() {
-          if ($scope.selectedCollege !== undefined) {
-
-            $scope.items = college[$scope.selectedCollege];
-
-            $scope.selectedItem;
-
-            if ($scope.selectedItem !== undefined) {
-               return $scope.selectedItem;
-            } 
-
-
-            return "Selected: " + $scope.selectedCollege;
-          } else {
-            return "Chose A College";
-          }
-        };
-
-
 }
 
 var college = 
