d7e2005a2f88c55b8d792538ff1006d2887a4846
Justin Besteman <justin@besteman.io>
2017-03-18 00:00:08 -0400

    working on the form.

--- src/selfStarted/features/components/createProjects/createProjects.directive.js
+++ src/selfStarted/features/components/createProjects/createProjects.directive.js
@@ -1,3 +1,5 @@
+
+
 angular
     .module('selfStarted.component.createProjects')
     .directive('createProjects', CreateProjects);
@@ -41,7 +43,180 @@ function CreateProjectsCtrl($scope){
           });
 
         // Configure a dark theme with primary foreground yellow
+       
 
+        $scope.colleges = Object.keys(college);
+        $scope.selectedCollege;
+        $scope.getSelectedText = function() {
+          if ($scope.selectedCollege !== undefined) {
+
+            $scope.items = college[$scope.selectedCollege];
+
+            $scope.selectedItem;
+
+            if ($scope.selectedItem !== undefined) {
+               return $scope.selectedItem;
+            } 
+
+
+            return "Selected: " + $scope.selectedCollege;
+          } else {
+            return "Chose A College";
+          }
+        };
 
 
 }
+
+var college = 
+
+  {
+    "Arts & Humanities": 
+
+      [
+        "Architecture",
+        "Art",
+        "Digital Media",
+        "English",
+        "Film",
+        "French",
+        "History",
+        "Humanities and Cultural Studies",
+        "Latin American Studies",
+        "Music",
+        "Philosophy",
+        "Photography",
+        "Religion and Cultural Studies",
+        "Spanish",
+        "Theatre",
+        "Writing and Rhetoric"
+      ],
+  
+  
+    "Business Administration" : 
+      [
+        "Accounting",
+        "Economics",
+        "Finance",
+        "Integrated Business",
+        "Management",
+        "Marketing",
+        "Real Estate"
+      ],
+
+   "Education & Human Performance" : 
+      [
+        "Art Education",
+        "Early Childhood Development",
+        "Elementary Education",
+        "English Language Arts Education",
+        "Mathematics Education",
+        "Science Education",
+        "Social Science Education",
+        "Sport and Exercise Education",
+        "Sport and Exercise Science",
+        "Technical Education and Industry Training",
+        "World Languages Education"
+      ],
+
+    "Engineering & Computer Science" :
+      [
+        "Aerospace Engineering",
+        "Civil Engineering",
+        "Computer Engineering",
+        "Computer Science",
+        "Construction Engineering",
+        "Electrical Engineering",
+        "Environmental Engineering",
+        "Industrial Engineering",
+        "Information Technology",
+        "Mechanical Engineering"
+      ],
+
+    "Engineering & Computer Science" :
+      [
+        "Aerospace Engineering",
+        "Civil Engineering",
+        "Computer Engineering",
+        "Computer Science",
+        "Construction Engineering",
+        "Electrical Engineering",
+        "Environmental Engineering",
+        "Industrial Engineering",
+        "Information Technology",
+        "Mechanical Engineering"
+      ],
+
+    "Health & Public Affairs":
+      [
+        "Athletic Training",
+        "Communication Sciences and Disorders",
+        "Criminal Justice",
+        "Health Informatics and Information Management",
+        "Health Sciences Pre-Clinical",
+        "Health Services Administration",
+        "Legal Studies",
+        "Public Administration",
+        "Social Work"
+      ],
+
+    "Hospitality Management":
+      [
+        "Entertainment Management",
+        "Event Management",
+        "Hospitality Management",
+        "Restaurant and Foodservice Management"
+      ],
+
+    "Medicine":
+      [
+        "Biomedical Sciences",
+        "Biotechnology",
+        "Medical Laboratory Sciences"
+      ],
+
+    "Nursing":
+      [
+        "Nursing"
+      ],
+
+    "Optics & Photonics":
+      [
+        "Photonic Science and Engineering"
+      ],
+
+    "Sciences":
+      [
+        "Advertising-Public Relations",
+        "Anthropology",
+        "Biology",
+        "Chemistry",
+        "Chemistry - Biochemistry",
+        "Communication & Conflict",
+        "Forensic Science - Analysis",
+        "Forensic Science - Biochemistry",
+        "Human Communication",
+        "International and Global Studies",
+        "Journalism",
+        "Mathematics",
+        "Physics",
+        "Political Science",
+        "Political Science - Prelaw",
+        "Psychology",
+        "Radio - Television",
+        "Social Sciences",
+        "Sociology",
+        "Statistics"
+      ],
+
+    "Undergraduate Studies":
+      [
+        "Interdisciplinary Studies",
+        "Interdisciplinary Studies - Environmental Studies",
+        "Interdisciplinary Studies - Women's Studies"
+      ]
+
+  }
+
+
+
