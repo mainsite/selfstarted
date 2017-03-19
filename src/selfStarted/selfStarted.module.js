978621a5cc710d6cf362903a3afd177a27f302dd
Justin Besteman <justin@besteman.io>
2017-03-19 12:11:10 -0400

    didn't know that I had to do a npm install

--- src/selfStarted/selfStarted.module.js
+++ src/selfStarted/selfStarted.module.js
@@ -2,6 +2,7 @@ angular
     .module('selfStarted', [
         'ui.router',
         'ui.bootstrap',
+        'ngAnimate',
         'selfStarted.landingPage',
         'selfStarted.dashboard'
     ])
