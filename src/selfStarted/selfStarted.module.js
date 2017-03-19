f3bc8de62c2c33dfda6ea6028ec8d4261f50899b
Justin Besteman <justin@besteman.io>
2017-03-19 09:09:10 -0400

    Fixed the errors by removing the ng

--- src/selfStarted/selfStarted.module.js
+++ src/selfStarted/selfStarted.module.js
@@ -2,7 +2,6 @@ angular
     .module('selfStarted', [
         'ui.router',
         'ui.bootstrap',
-        'ngAnimate',
         'selfStarted.landingPage',
         'selfStarted.dashboard'
     ])
