bf076aea868f3bad9f5ee40d978ee24942ae8439
realzaidiguy <realzaidiguy@gmail.com>
2017-03-21 22:41:34 -0400

    messages get route added

---
+++ api/routes/messages.api.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+const express = require('express');
+const router = express.Router();
+const Messages = require('../models/messages.model');
+
+router.get('/projects/searchMessages?', function(req, res, next) {
+
+    console.log(req.query);
+
+    Projects.find(req.query)
+        .populate('_fromUser')
+        .populate('_toUser')
+        .exec(function(error, messagesData) {
+            if (error) return error;
+            res.json(messagesData);
+        });
+});
+
+module.exports = router;
