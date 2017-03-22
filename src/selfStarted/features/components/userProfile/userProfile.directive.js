582d38c7e581b67d917568bdd4f3160f95335860
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-03-22 14:22:06 -0400

    Modal Displays user info

--- src/selfStarted/features/components/userProfile/userProfile.directive.js
+++ src/selfStarted/features/components/userProfile/userProfile.directive.js
@@ -21,12 +21,11 @@ function UserProfileCtrl($scope, $uibModalInstance, localStorageService, UsersSe
         _id: getItem('userDBid')
     };
 
-    console.log(userID);
-
     users.getUsers(userID, function (err, response) {
         if (err) console.error(err);
-
-        console.log(response);
+        var user = response[0];
+        console.log("User is:", user.firstName);
+        $scope.user = setCurrentUser(user);
     });
 
     $scope.ok = function () {
@@ -38,6 +37,21 @@ function UserProfileCtrl($scope, $uibModalInstance, localStorageService, UsersSe
         console.log("you clicked cancel");
         $uibModalInstance.close();
     };
+
+    function setCurrentUser(user) {
+        var local = user.willDoLocalProjects ? 'yes' : 'no';
+        var remote = user.willDoRemoteProjects ? 'yes' : 'no';
+        return {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            location: user.userLocation,
+            description: user.aboutMe,
+            url: user.linkedInURL,
+            photo: user.userPhotoLink,
+            local: local,
+            remote: remote
+        };
+    }
 
     function getItem(key) {
         return localStorageService.get(key);
