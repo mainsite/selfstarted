7fe7399cdc9d263819e111d1e57689498d3e6b6d
realzaidiguy <realzaidiguy@gmail.com>
2017-03-23 13:47:47 -0400

    remove favicon npm related code

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const auth = require('./api/routes/auth.route');
 const logger = require('morgan');
 const favicon = require('serve-favicon');
 const port = process.env.PORT || 3000;
-const path = require('path');
-
-app.use(favicon(path.join(__dirname, 'src', 'favicon.ico')));
 
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/src'));
