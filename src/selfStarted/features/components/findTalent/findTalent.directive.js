2f616ebd9ae20319819ca6f7d0c6c11fec5063a1
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-03-23 15:24:12 -0400

    merge files

--- src/selfStarted/features/components/findTalent/findTalent.directive.js
+++ src/selfStarted/features/components/findTalent/findTalent.directive.js
@@ -6,7 +6,7 @@ function FindTalent() {
     return {
         restrict: 'E',
         scope: {
-            todoData: '='
+            userData: '='
         },
         templateUrl: 'selfStarted/features/components/findTalent/findTalent.html',
         controller: FindTalentCtrl,
@@ -16,11 +16,12 @@ function FindTalent() {
 }
 
 
-function FindTalentCtrl($scope , UsersService) {
+function FindTalentCtrl($scope , UsersService, localStorageService, ProjectsService ) {
 
     var vm = this;
     var mainCollegeField = Object.keys(college);
     var users = UsersService;
+    var projects = ProjectsService;
 
     $scope.submit = submit;
     $scope.colleges = mainCollegeField;
@@ -36,11 +37,14 @@ function FindTalentCtrl($scope , UsersService) {
 
         $scope.subcolleges = userSub;
 
-        console.log($scope.subcolleges);
-
     }
 
     function submit() {
+
+        var userDBid = getItem('userDBid');
+
+      
+
         var firstName = $scope.firstName ? $scope.firstName.trim().toLowerCase() : undefined;
         var lastName = $scope.lastName ? $scope.lastName.trim().toLowerCase() : undefined;
 
@@ -51,15 +55,81 @@ function FindTalentCtrl($scope , UsersService) {
             defaultSkillByProgram: $scope.subCollege
         };
 
-        console.log(searchUsersInfo);
+       
 
         users.getUsers(searchUsersInfo, function (err, res) {
           if(err) return console.log(err);
-          $scope.projects = res;
-          console.log($scope.projects);
+          $scope.theUsers = res;
+         
+        });
+
+        function getItem(key) {
+            return localStorageService.get(key);
+        }
+
+        var theProjects = {
+
+            _primaryProjectOwner: userDBid
+
+        }
+
+        projects.getAllProjects(theProjects, function (err, res) {
+          if(err) return console.log(err);
+         
+            var holder = [];
+
+          for (var i = 0; i < res.data.length; i++) {
+              
+            holder.push(res.data[i].projectName);
+
+          }
+
+          console.log(holder);
+
+          $scope.userProjects = holder;
+         
         });
         
     }
+
+
+   
+        var panels = angular.element('.user-infos');
+        var panelsButton = angular.element('.dropdown-user');
+        panels.hide();
+
+        //Click dropdown
+        panelsButton.click(function() {
+            //get data-for attribute
+            var dataFor = angular.element(this).attr('data-for');
+            var idFor = angular.element(dataFor);
+
+            //current button
+            var currentButton = angular.element(this);
+            idFor.slideToggle(400, function() {
+                //Completed slidetoggle
+                if(idFor.is(':visible'))
+                {
+                    currentButton.html('<i class="icon-chevron-up text-muted"></i>');
+                }
+                else
+                {
+                    currentButton.html('<i class="icon-chevron-down text-muted"></i>');
+                }
+            })
+        });
+
+
+      
+
+        angular.element(".main").on("click",".recruit" ,function(e) {
+            e.preventDefault();
+            var userChoosen = angular.element(this).data("index");
+            console.log(userChoosen);
+
+        });
+  
+
 
 }
 
